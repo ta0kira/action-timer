46dbf78abfd8fc40b1a201fea7f69cc9186507ab
Kevin P. Barry <ta0kira@gmail.com>
2016-07-16 17:38:46 -0400

    Updated zombie cleanup in poisson-queue, and added more failures.

--- timer/poisson-queue.cpp
+++ timer/poisson-queue.cpp
@@ -18,15 +18,15 @@ namespace {
 using locked_processors =
   lc::locking_container <std::unordered_map <std::string, std::unique_ptr<queue_processor <int>>>, lc::dumb_lock>;
 
-void zombie_cleanup(locked_processors &processors) {
+void zombie_cleanup(locked_processors &processors, action_timer <std::string> &actions) {
   auto write_processors = processors.get_write();
   assert(write_processors);
   for (auto current = write_processors->begin(); current != write_processors->end();) {
     if (!current->second || current->second->is_terminated()) {
       auto removed = current++;
       std::cerr << "Cleaning up " << removed->first << "." << std::endl;
-      // TODO: Is there a race condition between this and action_timer's
-      // execution of the action when action_timer has multiple threads?
+      actions.set_category(removed->first, 0.0);
+      actions.set_action(removed->first, nullptr);
       write_processors->erase(removed);
     } else {
       ++current;
@@ -47,8 +47,9 @@ int main(int argc, char *argv[]) {
   actions.start();
 
   actions.set_category("zombie_cleanup", 1.0);
-  action_timer <std::string> ::generic_action zombie_action(new sync_action([&processors] {
-    zombie_cleanup(processors);
+  // NOTE: Needs to be async_action to avoid a deadlock!
+  action_timer <std::string> ::generic_action zombie_action(new async_action([&processors,&actions] {
+    zombie_cleanup(processors, actions);
     return true;
   }));
   actions.set_action("zombie_cleanup", std::move(zombie_action));
@@ -78,9 +79,11 @@ int main(int argc, char *argv[]) {
     if (lambda <= 0.0) {
       // NOTE: Removing a processor will result in the queued data being lost!
       // 1. Remove the category from consideration.
-      actions.set_category(category, lambda);
+      actions.set_category(category, 0.0);
+
       // 2. Remove the category's action.
       actions.set_action(category, nullptr);
+
       // 3. Remove the catgory's processor.
       write_processors->erase(category);
     } else {
@@ -90,13 +93,20 @@ int main(int argc, char *argv[]) {
       const unsigned int capacity = std::max(1, (int) lambda);
       std::unique_ptr<queue_processor <int>> processor(
         new queue_processor <int> ([category,lambda](int value) {
-                                     std::cout << category << ": " << value << std::endl;
-                                     std::this_thread::sleep_for(
-                                       std::chrono::duration <double> (0.9 / lambda));
-                                     return true;
-                                   }, capacity));
+                                    if (!((value + 1) % 1021)) {
+                                      // This isn't necessary; it just tests failure.
+                                      std::cerr << category << ": FAILURE" << std::endl;
+                                      return false;
+                                    }
+                                    std::cout << category << ": " << value << std::endl;
+                                    std::this_thread::sleep_for(
+                                      std::chrono::duration <double> (0.9 / lambda));
+                                    return true;
+                                  }, capacity));
+
       processor->start();
       auto *const processor_ptr = processor.get();
+
       action_timer <std::string> ::generic_action action(
         new sync_action([processor_ptr,category,&queue] {
                           if (!processor_ptr->transfer_next_item(queue)) {
@@ -107,13 +117,16 @@ int main(int argc, char *argv[]) {
                           }
                           return true;
                         }));
+
       // 2. Replace (or add) the action.
       actions.set_action(category, std::move(action));
+
       // 3. Replace (or add) the processor.
       // NOTE: This must come after set_action so that the old action is removed
       // before its processor is destructed!
       // TODO: Figure out why std::move + emplace causes a deadlock.
       (*write_processors)[category].swap(processor);
+
       // 4. Update (or add) the category for consideration.
       // TODO: Maybe this should be the only action if the processor already
       // exists? Might not work, since the processor's queue is based on lambda.
