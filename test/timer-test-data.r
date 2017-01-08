30a1c8d53049a604b334e8e102fd8ad4811d7974
Kevin P. Barry <ta0kira@gmail.com>
2017-01-08 13:00:41 -0500

    Adds plotting of expected wait time to timer-test-data.r.

--- test/timer-test-data.r
+++ test/timer-test-data.r
@@ -6,6 +6,43 @@ sink(stderr())
 l     <- 10000
 count <- 100000
 
+rev.sums <- function(x) rev(cumsum(rev(x)))
+
+cond.expected.wait <- function(x) {
+  x <- sort(x)
+  cbind(x,rev.sums(x)/rev.sums(rep(1,length(x)))-x)
+}
+
+plot.image <- function (log.min.sleep,data) {
+  png(paste('timer-data-plot',log.min.sleep,'.png',sep=''),width=800,height=800)
+  lim <- max(quantile(data$expected,0.99),quantile(data$actual,0.99))
+  plot(data[c(1,2)],pch='.',cex=3,xlim=c(0,lim),ylim=c(0,lim),asp=1,col=rgb(0,0,0,0.05))
+  lines(c(0,1),c(0,1),col=rgb(1,0,0,0.9),lwd=2)
+  dev.off()
+}
+
+hist.image <- function (log.min.sleep,data) {
+  png(paste('timer-data-hist',log.min.sleep,'.png',sep=''),width=800,height=800)
+  hist(data$actual,xlim=c(0,quantile(data$actual,0.99)),breaks=500,freq=FALSE)
+  lines.x <- (0:1000)/1000*quantile(data$actual,0.999)
+  lines(lines.x,exp(-lines.x*l)*l,col='red',lwd=3)
+  l.actual <- 1/mean(data$actual)
+  lines(lines.x,exp(-lines.x*l.actual)*l.actual,col='green',lwd=3)
+  dev.off()
+}
+
+wait.image <- function (log.min.sleep,data) {
+  png(paste('timer-data-wait',log.min.sleep,'.png',sep=''),width=800,height=800)
+  wait.for.actual <- cond.expected.wait(data$actual)
+  wait.for.expected <- cond.expected.wait(data$expected)
+  lim <- quantile(data$actual,0.999)
+  plot(wait.for.expected,pch='.',cex=3,xlim=c(0,lim),ylim=c(0,2/l),
+       col=rgb(0,0,0.75,0.05),xlab='actual',ylab='expected wait')
+  points(wait.for.actual,pch='.',col=rgb(0,0,0,0.05))
+  lines(c(min(data$expected),max(data$expected)),rep(1/l,2),col=rgb(1,0,0,0.9),lwd=2)
+  dev.off()
+}
+
 for (log.min.sleep in 2:6) {
   # NOTE: Calling format with all three at once will turn l and count into
   # floating-points.
@@ -30,17 +67,7 @@ for (log.min.sleep in 2:6) {
   mean.diff <- mean(data$error)
   write(paste('  mean expected vs. actual:     ',format(mean.diff)),file=standard_out)
 
-  png(paste('timer-data-plot',log.min.sleep,'.png',sep=''),width=800,height=800)
-  lim <- max(quantile(data$expected,0.99),quantile(data$actual,0.99))
-  plot(data[c(1,2)],pch='.',cex=3,xlim=c(0,lim),ylim=c(0,lim),asp=1,col=rgb(0,0,0,0.05))
-  lines(c(0,1),c(0,1),col='red',lwd=2)
-  dev.off()
-
-  png(paste('timer-data-hist',log.min.sleep,'.png',sep=''),width=800,height=800)
-  hist(data$actual,xlim=c(0,quantile(data$actual,0.99)),breaks=500,freq=FALSE)
-  lines.x <- (0:1000)/1000*quantile(data$actual,0.999)
-  lines(lines.x,exp(-lines.x*l)*l,col='red',lwd=3)
-  l.actual <- 1/mean(data$actual)
-  lines(lines.x,exp(-lines.x*l.actual)*l.actual,col='green',lwd=3)
-  dev.off()
+  plot.image(log.min.sleep,data)
+  hist.image(log.min.sleep,data)
+  wait.image(log.min.sleep,data)
 }
